dd0c17b69d9a59e6c9a6e729b45b9c4e2e2cd236
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-06-06 20:56:16 +0200

    checking if sintax is ok and adding comments

--- script.js
+++ script.js
@@ -39,53 +39,53 @@
     newButton.appendChild(newButtonnode);
     divBtn.append(newButton);
 
-    //creating audio element  and appending it to body
+    /* /creating audio element  and appending it to body
    const audio = document.createElement("audio");
    audio.preload = "auto";
    audio.src = "https://themushroomkingdom.net/sounds/wav/smb/smb_coin.wav";
-   document.body.appendChild(audio);
+   document.body.appendChild(audio);*/
 
     // *** styling elements and page *** \\\
 
    /// ***styling body*** \\\
    // margin and vh body
-   document.body.style.margin="0 auto"
-   document.body.style.height="100vh"
+   document.body.style.margin="0 auto";
+   document.body.style.height="100vh";
    //changing font family for all the elements in body
-   document.body.style.fontFamily="sans-serif"
+   document.body.style.fontFamily="sans-serif";
    // changing background
-   document.body.style.backgroundColor = "lightgrey"
+   document.body.style.backgroundColor = "lightgrey";
 
    /// ***styling container*** \\\
    //making border div
-   divContainer.style.border="5px solid black"
-   divContainer.style.borderRadius="25%"
-   divContainer.style.borderBlock="none"
+   divContainer.style.border="5px solid black";
+   divContainer.style.borderRadius="25%";
+   divContainer.style.borderBlock="none";
    //giving height and width container
-   divContainer.style.height = "350px"
-   divContainer.style.width = "350px"
+   divContainer.style.height = "350px";
+   divContainer.style.width = "350px";
    // flexbox ond parent div
-   divContainer.style.display="flex"
-   divContainer.style.alignItems="center"
-   divContainer.style.justifyContent="center"
-   divContainer.style.flexDirection= "column"
+   divContainer.style.display="flex";
+   divContainer.style.alignItems="center";
+   divContainer.style.justifyContent="center";
+   divContainer.style.flexDirection= "column";
    // putting div in the middle of page
-   divContainer.style.margin="120px auto"
+   divContainer.style.margin="120px auto";
 
    //styling title
-   newH1.style.fontSize="50px"
+   newH1.style.fontSize="50px";
 
    //styling p tag
-   newP.style.fontSize="20px"
+   newP.style.fontSize="20px";
 
    //styling button
-   newButton.style.height="50px"
-   newButton.style.borderRadius="20%"
-   newButton.style.fontStyle="italic"
-   newButton.style.cursor="pointer"
-   newButton.style.background="transparent"
-   newButton.style.border="1px solid black"
-   newButton.style.color="black"
+   newButton.style.height="50px";
+   newButton.style.borderRadius="20%";
+   newButton.style.fontStyle="italic";
+   newButton.style.cursor="pointer";
+   newButton.style.background="transparent";
+   newButton.style.border="1px solid black";
+   newButton.style.color="black";
 
 
    // *** making function and event listener to generate random color *** \\\
@@ -93,34 +93,43 @@
    function generateRandomColor() {
        const letters = '0123456789ABCDEF';
        let color = '#';
+       /*let colorTitle = '#';
+       let colorP = '#';
+       let colorBorder = '#';*/
        for (let i = 0; i < 6; i++) {
            color += letters[Math.floor(Math.random() * 16)];
            document.body.style.background= color;
            newP.innerText=`Background color: Hex rgb ${color}`;
+
+           /*colorTitle+= letters[Math.floor(Math.random() * 16)];
+           newH1.style.color=colorTitle;
+
+           colorBorder+= letters[Math.floor(Math.random() * 16)];
+           divContainer.style.border=`5px solid ${colorBorder}`;
+
+           colorP+= letters[Math.floor(Math.random() * 16)];
+           newP.style.color=colorP;*/
        }
    }
-
-   // *** making hover effect with eventlistener and functions when you hover the button *** \\\
+   // *** making hover effect with event listener and functions when you hover the button *** \\\
    newButton.addEventListener("mouseover", mouseOver);
    newButton.addEventListener("mouseout", mouseOut);
 
    function mouseOver() {
-       newButton.style.background= "black"
+       newButton.style.background= "black";
        newButton.style.color = "white";
    }
 
    function mouseOut() {
-       newButton.style.background= "transparent"
+       newButton.style.background= "transparent";
        newButton.style.color = "black";
    }
    // *** making event listener for button, every time you click color will change and the hex rgb code will display *** \\\
         newButton.addEventListener('click',function (){
-            generateRandomColor()
-            audio.play();
+            generateRandomColor();
+            //audio.play();
             //setInterval(generateRandomColor,100)
         })
-
-
 
 
 
